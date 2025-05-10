d5b5b22490168505c655246ba59dee2c35cad9e2
Ricardo Sanjines <sanjglobalcorp@gmail.com>
2025-05-09 23:29:29 -0400

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is in C#");
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
     }
 }
