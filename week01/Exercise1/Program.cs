9a8b0000ae83a35b80e2a0fa5517e0dca3e71106
Ricardo Sanjines <sanjglobalcorp@gmail.com>
2025-05-13 19:06:49 -0400

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,13 +5,12 @@ class Program
     static void Main(string[] args)
     {
 
-            // Ask the user for their first name
-            Console.Write("What is your first name? ");
-            string firstName = Console.ReadLine();
-            // Ask the user for their last name
-            Console.Write("What is your last name? ");
-            string lastName = Console.ReadLine();
-            // Display the formatted full name in the specific format
-            // Example: "Your name is Burton, Scott Burton."
-            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+     Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
 }
