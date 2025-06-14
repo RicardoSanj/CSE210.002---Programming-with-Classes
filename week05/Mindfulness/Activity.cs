cf8da2cd81e18a38a285dca6023ffe86d770e688
Ricardo Sanjines <sanjglobalcorp@gmail.com>
2025-06-13 22:45:57 -0400

    Update Activity.cs

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -13,53 +13,60 @@ public abstract class Activity
         _description = description;
     }
 
-    public void Start()
+    public void DisplayStartingMessage()
     {
         Console.Clear();
-        Console.WriteLine($"Starting {_name} Activity");
+        Console.WriteLine($"--- {_name} ---");
+        Console.WriteLine();
         Console.WriteLine(_description);
+        Console.WriteLine();
         Console.Write("Enter duration in seconds: ");
-        _duration = int.Parse(Console.ReadLine());
-        Console.WriteLine("Get ready to begin...");
-        ShowSpinner(3);
-        PerformActivity();
-        End();
-    }
-
-    protected abstract void PerformActivity();
-
-    protected void End()
-    {
-        Console.WriteLine("Good job!");
-        ShowSpinner(2);
-        Console.WriteLine($"You completed {_name} Activity for {_duration} seconds.");
-        ShowSpinner(3);
-    }
-
-    protected int GetDuration() => _duration;
-
-    protected void ShowSpinner(int seconds)
-    {
-        string[] spinner = { "|", "/", "-", "\" };
-        DateTime end = DateTime.Now.AddSeconds(seconds);
-        int i = 0;
-
-        while (DateTime.Now < end)
+        while (!int.TryParse(Console.ReadLine(), out _duration) || _duration <= 0)
         {
-            Console.Write(spinner[i % 4]);
+            Console.Write("Invalid input. Enter a positive integer for duration: ");
+        }
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Well done!");
+        ShowSpinner(3);
+        Console.WriteLine($"You have completed the {_name} for {_duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "/", "-", "\\", "|" };
+        DateTime startTime = DateTime.Now;
+        int i = 0;
+        while ((DateTime.Now - startTime).TotalSeconds < seconds)
+        {
+            Console.Write(spinner[i]);
             Thread.Sleep(250);
-            Console.Write("");
-            i++;
+            Console.Write("\b");
+            i = (i + 1) % spinner.Length;
         }
     }
 
-    protected void Countdown(int seconds)
+    public void ShowCountdown(int seconds)
     {
         for (int i = seconds; i > 0; i--)
         {
             Console.Write(i);
             Thread.Sleep(1000);
-            Console.Write(" ");
+            Console.Write("\b \b");
         }
     }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    // Método abstracto que obliga a las clases derivadas a implementarlo
+    public abstract void PerformActivity();
 }
