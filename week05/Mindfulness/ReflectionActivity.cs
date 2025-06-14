b7319787472dbea11c5f2fe96b9b931a0100c3a8
Ricardo Sanjines <sanjglobalcorp@gmail.com>
2025-06-13 22:48:47 -0400

    Update ReflectionActivity.cs

--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -1,48 +1,56 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 
 public class ReflectionActivity : Activity
 {
-    private List<string> _prompts = new List<string>
-    {
+    private string[] _prompts = {
         "Think of a time when you stood up for someone else.",
         "Think of a time when you did something really difficult.",
         "Think of a time when you helped someone in need.",
         "Think of a time when you did something truly selfless."
     };
 
-    private List<string> _questions = new List<string>
-    {
+    private string[] _questions = {
         "Why was this experience meaningful to you?",
         "Have you ever done anything like this before?",
         "How did you get started?",
         "How did you feel when it was complete?",
-        "What made this time different than other times?",
+        "What made this time different than other times when you were not as successful?",
         "What is your favorite thing about this experience?",
-        "What could you learn from this experience?",
-        "What did you learn about yourself?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
         "How can you keep this experience in mind in the future?"
     };
 
-    public ReflectionActivity()
-        : base("Reflection", "This activity will help you reflect on times when you have shown strength and resilience.")
+    public ReflectionActivity() : base(
+        "Reflection Activity",
+        "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life."
+    )
     {
     }
 
-    protected override void PerformActivity()
+    public override void PerformActivity()
     {
-        Random rand = new Random();
-        Console.WriteLine(_prompts[rand.Next(_prompts.Count)]);
-        Console.WriteLine("Reflect on the following questions:");
-        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+        DisplayStartingMessage();
 
-        while (DateTime.Now < endTime)
+        Random rnd = new Random();
+        Console.WriteLine();
+        Console.WriteLine(_prompts[rnd.Next(_prompts.Length)]);
+        Console.WriteLine("Press Enter when you have something in mind.");
+        Console.ReadLine();
+
+        int duration = GetDuration();
+        DateTime startTime = DateTime.Now;
+        int questionIndex;
+
+        while ((DateTime.Now - startTime).TotalSeconds < duration)
         {
-            string question = _questions[rand.Next(_questions.Count)];
-            Console.WriteLine(question);
-            ShowSpinner(4);
+            questionIndex = rnd.Next(_questions.Length);
             Console.WriteLine();
+            Console.WriteLine(_questions[questionIndex]);
+            ShowSpinner(5);
         }
+
+        DisplayEndingMessage();
     }
 }
