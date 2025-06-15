5f2e75b09f33ca50a756f41da82df05f741759cd
Ricardo Sanjines <sanjglobalcorp@gmail.com>
2025-06-15 00:09:24 -0400

    Update BreathingActivity.cs

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,24 +1,29 @@
-
 using System;
+using System.Threading;
 
 public class BreathingActivity : Activity
 {
-    public BreathingActivity()
+    public BreathingActivity() : base("Breathing Activity",
+        "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
     {
-        SetName("Breathing");
-        SetDescription("This activity will help you relax by guiding you through slow breathing.");
     }
 
     public override void PerformActivity()
     {
         DisplayStartingMessage();
-        for (int i = 0; i < GetDuration(); i += 8)
+        int duration = GetDuration();
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+
+        while (DateTime.Now < endTime)
         {
-            Console.WriteLine("Breathe in...");
-            ShowCountdown(4);
-            Console.WriteLine("Breathe out...");
-            ShowCountdown(4);
+            Console.Write("Breathe in... ");
+            Pause(4);
+            Console.WriteLine();
+            Console.Write("Breathe out... ");
+            Pause(6);
+            Console.WriteLine();
         }
+
         DisplayEndingMessage();
     }
 }
